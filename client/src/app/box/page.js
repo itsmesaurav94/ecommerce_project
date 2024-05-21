9bafaf5c4082672855b97245593a5ec1ee4f57dc
Saurav Pokharel <itsmesaurav94@gmail.com>
2024-05-21 08:48:32 +0545

    modified box slice

--- client/src/app/box/page.js
+++ client/src/app/box/page.js
@@ -3,10 +3,13 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const Box = () => {
-    const {backgroundColor, height, width} = useSelector(state=>state.box)
+    const {height, width, backgroundColor} = useSelector(state=>state.box)
   return (
     <div>
-        <div ></div>
+      <div style={{backgroundColor: backgroundColor, height: height, width:width }}>
+        
+      </div>
+
     </div>
   )
 }
