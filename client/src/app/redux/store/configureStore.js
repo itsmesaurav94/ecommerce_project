0730e7e3e9693414415ee7e9455293b9e2d15250
Saurav Pokharel <itsmesaurav94@gmail.com>
2024-05-29 10:51:26 +0545

    redux persist install error

--- client/src/app/redux/store/configureStore.js
+++ client/src/app/redux/store/configureStore.js
@@ -1,15 +1,22 @@
-import counterSlice from "../reducerSlices/counterSlice";
-import boxSlice from "../reducerSlices/boxSlice";
+import { configureStore, Tuple, combineReducers } from '@reduxjs/toolkit'
+import logger from 'redux-logger'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import boxSlice from '../reducerSlices/boxSlice';
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+  const rootReducer = combineReducers({ 
+    box: boxSlice,
 
-import { configureStore, Tuple } from '@reduxjs/toolkit'
-import logger from "redux-logger";
+  })
 
-const store = configureStore({
-    reducer:{
-        counter: counterSlice,
-        box: boxSlice
-    },
-    middleware: () => new Tuple(logger),
+  const persistedReducer = persistReducer(persistConfig, rootReducer)  
+export const store = configureStore({
+    reducer: persistedReducer,
+  middleware: () => new Tuple( logger),
+
 })
 
-export default store
+export const persistor = persistStore(store)
