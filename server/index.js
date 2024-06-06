2cefdc18682e08641b743fcf913960effc730c18
Saurav Pokharel <itsmesaurav94@gmail.com>
2024-06-06 08:09:51 +0545

    login step underway

--- server/index.js
+++ server/index.js
@@ -1,45 +1,70 @@
 const express = require('express')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const dbConnect = require('./src/db/connection')
 dbConnect()
-// const bcrypt = require('bcrypt');
-// const saltRounds = 10;
 const app = express()
 require('dotenv').config()
+//body parser
 app.use(express.json())
 const mongoose = require('mongoose')
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  fullName: String, // String is shorthand for {type: String}
-  phoneNumber: String,
+  phoneNumber: String, // String is shorthand for {type: String}
+  fullName: String,
   email: String,
   password: String,
   gender: {
     type: String,
-    enum: ['male', 'female','other'],
+    enum : ['male','female','other'],
     default: 'female'
   },
   role: {
     type: String,
-    enum: ['admin', 'user'],
+    enum : ['admin','user'],
     default: 'user'
   },
-  
 });
-
-
 const User = mongoose.model('User', userSchema);
-const port = process.env.PORT
+const port = process.env.PORT || 8000
 
+app.post('/register', async (req, res) => {
+  const hashPassword = await bcrypt.hash(req.body.password, saltRounds)
+  req.body.password = hashPassword
+  const phoneExist  = await User.exists({phoneNumber: req.body.phoneNumber})
+  const emailExist  = await User.exists({email: req.body.email})
 
-app.post('/register', (req, res) => {
-   User.create(req.body)
-  res.send('ok')
+  if(phoneExist ){
+   return res.json({msg: "Phone Number is taken!"})
+  }else if(emailExist){
+    return res.json({msg: "Email is taken!"})
+  }
+  await User.create(req.body)
+  return res.json({msg: "User registered"})
 })
 
-app.get('/users', async (req, res) => {
+app.post('/login',async(req,res)=>{
+  console.log(req.body)
+  //STEP 1:
+  const phoneExist  = await User.exists({phoneNumber: req.body.phoneNumber})
+  //check if phone number exist
+  if(phoneExist){
+
+  }else{
+    res.json({msg:"Phone number not registered"})
+  }
+  // NO: res.json({msg: "User not registered"})
+  // YES: 
+    // check if password matches
+      // NO: res.json({msg: "Incorrect password"})
+      //YES: token
+})
+
+app.get('/users',async(req,res)=>{
   const data = await User.find()
-  res.send(data)
+  res.json(data)
 })
 
 app.listen(port, () => {
