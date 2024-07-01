60aceb721ff710306cfa5e09c1f72dc09c59361f
Saurav Pokharel <itsmesaurav94@gmail.com>
2024-07-01 08:04:48 +0545

    login success; dashboard begin

--- client/src/app/dashboard/page.js
+++ client/src/app/dashboard/page.js
@@ -1,27 +1,23 @@
-'use client'
-import NavBar from '@/components/navbar/page'
-import SideBar from '@/components/sidebar/page'
-import React from 'react'
-import MainSection from './mainSection'
-import LoginSection from './loginSection'
-import { useSelector } from 'react-redux'
-
+"use client";
+import NavBar from "@/components/navbar/page";
+import SideBar from "@/components/sidebar/page";
+import React from "react";
+import MainSection from "./mainSection";
+import LoginSection from "./loginSection";
+import { useSelector } from "react-redux";
 
 const page = () => {
-  const value = useSelector(state=> state.counter.value)
-
   return (
     <div>
-        <NavBar/>
-        <div className='flex'>
-            <SideBar/>
-            {value}
-            <LoginSection/>
-            <MainSection/>
-        </div>
-       
-    </div>
-  )
-}
+      <NavBar />
+      <div className="flex">
+        <SideBar />
 
-export default page
+        <LoginSection />
+        <MainSection />
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,80 +1,84 @@
-'use client'
+"use client";
 import React from "react";
-import {Tabs, Tab, Input, Link, Button, Card, CardBody, CardHeader} from "@nextui-org/react";
-import { useFormik } from 'formik';
+import {
+  Tabs,
+  Tab,
+  Input,
+  Link,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+} from "@nextui-org/react";
+import { useFormik } from "formik";
 import Image from "next/image";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
-
 export default function Main() {
-  const router = useRouter()
+  const router = useRouter();
   const [selected, setSelected] = React.useState("login");
   const formik = useFormik({
     initialValues: {
-      email: '',
-      fullName: '',
-      phoneNumber: '',
-      password: '',
+      email: "",
+      fullName: "",
+      phoneNumber: "",
+      password: "",
     },
-    onSubmit: values => {
-      registerUser(values)
+    onSubmit: (values) => {
+      registerUser(values);
     },
   });
-
-
 
   const formikLogin = useFormik({
     initialValues: {
-     
-      
-      phoneNumber: '',
-      password: '',
+      phoneNumber: "",
+      password: "",
     },
-    onSubmit: values => {
-      loginUser(values)
+    onSubmit: (values) => {
+      loginUser(values);
     },
   });
 
-
-
-  const registerUser = async(values)=>{
+  const registerUser = async (values) => {
     const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(values)
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    };
+    const response = await fetch(
+      "http://localhost:4000/register",
+      requestOptions
+    );
+    const data = await response.json();
+    if (response.statusText == "OK") {
+      toast.success(data.msg);
+      setSelected("login");
+    } else {
+      toast.error(data.msg);
+    }
   };
-  const response = await fetch('http://localhost:4000/register', requestOptions);
-  const data = await response.json()
-  if(response.statusText == 'OK'){
-    toast.success(data.msg)
-    setSelected('login')
-  }else{
-    toast.error(data.msg)
-  }
 
-
-  const loginUser = async(values)=>{
+  const loginUser = async (values) => {
     const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(values)
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    };
+    const response = await fetch("http://localhost:4000/login", requestOptions);
+    const data = await response.json();
+    if (response.statusText == "OK") {
+      toast.success(data.msg);
+      router.push("/dashboard");
+    } else {
+      toast.error(data.msg);
+    }
   };
-  const response = await fetch('http://localhost:4000/login', requestOptions);
-  const data = await response.json()
-  if(response.statusText == 'OK'){
-    toast.success(data.msg)
-    router.push('/dashboard')
-  }else{
-    toast.error(data.msg)
-  }
-}
 
-  }
   return (
     <div className="flex flex-col w-full ">
       <Card className=" flex self-center max-w-full w-[340px] ">
-    <Image src= "/logo.png" width={100} height={100} alt="logo"/>
+        <Image src="/logo.png" width={100} height={100} alt="logo" />
         <CardBody className="overflow-hidden">
           <Tabs
             fullWidth
@@ -84,17 +88,23 @@ export default function Main() {
             onSelectionChange={setSelected}
           >
             <Tab key="login" title="Login">
-              <form onSubmit={formikLogin.handleSubmit} className="flex flex-col gap-4">
-              <Input
-               name="phoneNumber"
-               onChange={formikLogin.handleChange}
-               value={formikLogin.values.phoneNumber}
-              isRequired label="Phone Number" placeholder="Enter your phoneNumber"  />
-              
+              <form
+                onSubmit={formikLogin.handleSubmit}
+                className="flex flex-col gap-4"
+              >
                 <Input
-                   name="password"
-                   onChange={formikLogin.handleChange}
-                   value={formikLogin.values.password}
+                  name="phoneNumber"
+                  onChange={formikLogin.handleChange}
+                  value={formikLogin.values.phoneNumber}
+                  isRequired
+                  label="Phone Number"
+                  placeholder="Enter your phoneNumber"
+                />
+
+                <Input
+                  name="password"
+                  onChange={formikLogin.handleChange}
+                  value={formikLogin.values.password}
                   isRequired
                   label="Password"
                   placeholder="Enter your password"
@@ -113,28 +123,41 @@ export default function Main() {
                 </div>
               </form>
             </Tab>
- 
+
             <Tab key="sign-up" title="Sign up">
-              <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4 ">
-              <Input
-               name="phoneNumber"
-               onChange={formik.handleChange}
-               value={formik.values.phoneNumber}
-              isRequired label="Phone Number" placeholder="Enter your phoneNumber"  />
-              <Input
-                 name="fullName"
-                 onChange={formik.handleChange}
-                 value={formik.values.fullName}
-               isRequired label="fullName" placeholder="Enter your fullName" />
+              <form
+                onSubmit={formik.handleSubmit}
+                className="flex flex-col gap-4 "
+              >
+                <Input
+                  name="phoneNumber"
+                  onChange={formik.handleChange}
+                  value={formik.values.phoneNumber}
+                  isRequired
+                  label="Phone Number"
+                  placeholder="Enter your phoneNumber"
+                />
+                <Input
+                  name="fullName"
+                  onChange={formik.handleChange}
+                  value={formik.values.fullName}
+                  isRequired
+                  label="fullName"
+                  placeholder="Enter your fullName"
+                />
                 <Input
                   name="email"
                   onChange={formik.handleChange}
                   value={formik.values.email}
-                isRequired label="Email" placeholder="Enter your email" type="email" />
+                  isRequired
+                  label="Email"
+                  placeholder="Enter your email"
+                  type="email"
+                />
                 <Input
-                   name="password"
-                   onChange={formik.handleChange}
-                   value={formik.values.password}
+                  name="password"
+                  onChange={formik.handleChange}
+                  value={formik.values.password}
                   isRequired
                   label="Password"
                   placeholder="Enter your password"
